2a294caf8ef477f9648410bf0b2e1628c0871b25
josef-code <jbbliss9@gmail.com>
2024-04-18 16:52:30 +0100

    add register feature

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,29 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import './../App.css';
 import { Helmet } from "react-helmet";
+import { supabase } from "../../supabaseClient";
 
 function Login() {
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  
+  const handleSubmit = async (e) => {
+      e.preventDefault();
 
+      try {
 
+        const { data, error } = await supabase.auth.signUp({
+            email: email,
+            password: password,
+        })
+
+        console.log(data);
+
+      } catch (error) {
+        console.log(error);
+      }
+  };
 
 
   return (
@@ -16,19 +34,23 @@ function Login() {
       <div class="flex items-center justify-center h-screen">
         <div class="bg-white p-8 rounded shadow-md w-80">
           <h2 class="text-2xl font-semibold mb-4">Login</h2>
-          <form>
+
+          <form onSubmit={handleSubmit}>
             <div class="mb-4">
               <label
                 for="username"
                 class="block text-gray-700 font-medium mb-1"
               >
-                Username
+                Email
               </label>
               <input
-                type="text"
-                id="username"
-                name="username"
+                type="email"
+                id="email"
+                name="email"
+                placeholder="email@example.com"
                 class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                onChange={(e) => setEmail(e.target.value)}
+                required
               />
             </div>
             <div class="mb-4">
@@ -42,7 +64,10 @@ function Login() {
                 type="password"
                 id="password"
                 name="password"
+                placeholder="123456"
                 class="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+                onChange={(e) => setPassword(e.target.value)}
+                required
               />
             </div>
             <button
@@ -53,7 +78,9 @@ function Login() {
             </button>
             
           </form>
+
           <a href="/register" class="w-full text-blue-800 font-medium">Don't have an account? Register</a>
+
         </div>
       </div>
     </>
---
+++ src/pages/dashboard.jsx
@@ -0,0 +1,16 @@
+import { Helmet } from "react-helmet";
+
+
+function Dashboard() {
+    
+    return (
+        <>
+            <Helmet>
+                <title>Dashboard - Fedbak</title>
+            </Helmet>
+            <h1>Dashboard</h1>
+        </>
+    );
+}
+
+export default Dashboard;
